711d1ad299504a0fcde153f287c297b1fc1fa6ec
whkakrkr <monk0613@naver.com>
2021-03-10 14:11:51 +0900

    21.03.10 - 2주차 실습 - git hub 처음으로 연동함

---
+++ Lab1/src/AddNumbers.java
@@ -0,0 +1,14 @@
+
+public class AddNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum = 0, i;
+		for (i=1; i<=10; i++) {
+			sum = sum + i;
+		}
+		System.out.println("1-10ÀÇ ÇÕ°è(sum) = " + sum);
+
+	}
+
+}
